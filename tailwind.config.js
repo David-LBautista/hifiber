bfbc06905d1669b1fb1d28e543280ddea6a4453a
David LB <48845853+David-LBautista@users.noreply.github.com>
2024-04-02 19:36:00 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      display: ["hover", "focus"],
+      visibility: ["hover", "focus"],
+    },
+  },
+  plugins: [],
+};
